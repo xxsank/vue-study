8777cdf8258b4549cad831d1d591ee960b6f31d9
sangkey <skyee23@gmail.com>
2019-07-25 16:13:49 +0900

    포스트 리스트 컴포넌트

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,25 +1,13 @@
 <template lang="pug">
   .page-home
-    section.featured-posts
-      post-preview(
-        id="1",
-        title="빅 데이터",
-        thumbnail="//goo.gl/mJ5Vsy",
-        content="빅 데이터란? 기존 데이터베이스 관리도구의 능력을 넘어서는 대량의 정형 또는 비정형 데이터 집합을 포함한 데이터로부터 가치를 추출하고 결과를 분석하는 기술이다."
-      )
-      post-preview(
-        id="2",
-        title="머신 러닝",
-        thumbnail="//goo.gl/HoiVkE",
-        content="머신 러닝은 인공 지능의 한 분야로, 컴퓨터가 학습할 수 있도록 하는 알고리즘과 기술을 개발하는 분야를 말한다."
-      )
+    post-list
 </template>
 
 <script>
-import PostPreview from '@/components/Posts/PostPreview'
+import PostList from '@/components/Posts/PostList'
 
 export default {
-  components: { PostPreview }
+  components: { PostList }
 }
 </script>
 
@@ -34,10 +22,7 @@ a
   &:active .post-content
     background-color: rgba($color, 0.26)
 
-.featured-posts
-  display: flex
-  justify-content: center
-  align-items: center
-  flex-wrap: wrap
-  padding: 20px
+@media (min-width: 768px)
+  .intro h1
+    font-size: 2rem
 </style>
